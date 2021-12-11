0efa88854606da6f9f1ab3c835633ecf9bb6bed1
asadcop <asadujjaman1122@gmail.com>
2021-12-11 19:45:35 +0600

    8th commit

---
+++ atcoder/Panasonic Programming Contest 2021(AtCoder Beginner Contest 231)/A - Water Pressure.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string a[100];
+    int n;
+    cin>>n;
+    int as[n];
+    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j]&&a[i]!="-1")
+            {   
+                a
+            }
+        }
+    }
+    return 0;
+}
