413549578382cd7680558a254ef55f5f6c0e1e5e
asadcop <asadujjaman1122@gmail.com>
2022-01-08 11:13:41 +0600

    12 commit

---
+++ codechif/CodeChef Starters 21 Division 3 (Rated)/Covid and Theatre Tickets.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int t;
+  cin>>t;
+  while (t--)
+  {
+    int a,b;
+    cin>>a>>b;
+    cout<<((b+1)/2)*((a+1)/2)<<endl;
+  }
+  
+  return 0;
+}
